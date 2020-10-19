8d8882f1399c8bebc477d1fcdc7a0b4daa72e15d
Bill Bensing <billbensing@wb3tech.com>
2020-10-18 20:22:00 -0400

    made clean strings function public

--- strings/stringDefaults.go
+++ strings/stringDefaults.go
@@ -2,8 +2,8 @@ package strings
 
 import "strings"
 
-// cleanString removes all leading & trailing spaces, or line breaks, in a string,
+// CleanString removes all leading & trailing spaces, or line breaks, in a string,
 // while retaining any other spaces or line breaks.
-func cleanString(value string) string {
+func CleanString(value string) string {
 	return strings.TrimSpace(value)
 }
